e0270a22b0913f3c6928b62cf2013e2e38cc866c
Alexandre Lopez <alexxandre.lopez@gmail.com>
2025-08-25 12:34:32 +0200

    Cleaned up unused packages and explicitly added transitive packages.

--- app/application.py
+++ app/application.py
@@ -11,7 +11,6 @@ import numpy as np
 
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify, session
 from flask_session import Session
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
